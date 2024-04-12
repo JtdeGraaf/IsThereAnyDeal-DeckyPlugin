7d7cc4dcea924049d488c3ccb3a2e4741cb9c2e5
JtdeGraaf <jt.graaf@gmail.com>
2024-04-12 15:57:28 +0200

    Remove all components from index

---
+++ src/components/DeckyMenuOption.tsx
@@ -0,0 +1,45 @@
+import { ButtonItem, Menu, MenuItem, PanelSection, PanelSectionRow, ServerAPI, showContextMenu } from 'decky-frontend-lib'
+import React from 'react'
+import logo from "../assets/logo.png";
+
+interface Props {
+    serverAPI: ServerAPI
+}
+
+const DeckyMenuOption = ({serverAPI}: Props) => {
+    return (
+        <PanelSection title="Panel Section">
+          <PanelSectionRow>
+            <ButtonItem
+              layout="below"
+              onClick={(e) =>
+                showContextMenu(
+                  <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
+                    <MenuItem onSelected={() => {}}>Item #1</MenuItem>
+                    <MenuItem onSelected={() => {}}>Item #2</MenuItem>
+                    <MenuItem onSelected={() => {}}>Item #3</MenuItem>
+                  </Menu>,
+                  e.currentTarget ?? window
+                )
+              }
+            >
+              Server says yolo
+            </ButtonItem>
+          </PanelSectionRow>
+  
+          <PanelSectionRow>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <img src={logo} />
+            </div>
+          </PanelSectionRow>
+  
+          <PanelSectionRow>
+            <ButtonItem>
+              Router
+            </ButtonItem>
+          </PanelSectionRow>
+        </PanelSection>
+      );
+}
+
+export default DeckyMenuOption
---
+++ src/components/PriceComparison.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PriceComparison = () => {
+  return (
+    <div>PriceComparison</div>
+  )
+}
+
+export default PriceComparison
