ef7237ca710e2c0042e43559fa33e1add14e6998
JtdeGraaf <jt.graaf@gmail.com>
2024-04-15 19:34:56 +0200

    Remove UIComposition since it was unneccesary

--- src/components/PriceComparison.tsx
+++ src/components/PriceComparison.tsx
@@ -3,36 +3,9 @@ import { useEffect, useState } from 'react'
 import { globalStates } from '../utils/GlobalStates';
 import { isThereAnyDealService } from '../service/IsThereAnyDealService';
 
-enum UIComposition {
-  Hidden = 0,
-  Notification = 1,
-  Overlay = 2,
-  Opaque = 3,
-  OverlayKeyboard = 4,
-}
-
-const useUIComposition: (composition: UIComposition) => void = findModuleChild(
-  (m) => {
-    if (typeof m !== "object") return undefined;
-    for (let prop in m) {
-      if (
-        typeof m[prop] === "function" &&
-        m[prop].toString().includes("AddMinimumCompositionStateRequest") &&
-        m[prop].toString().includes("ChangeMinimumCompositionStateRequest") &&
-        m[prop].toString().includes("RemoveMinimumCompositionStateRequest") &&
-        !m[prop].toString().includes("m_mapCompositionStateRequests")
-      ) {
-        return m[prop];
-      }
-    }
-  }
-);
-
-
 const PriceComparison = () => {
   const [appId, setAppid] = useState()
   const [label, setLabel] = useState("")
-  useUIComposition(appId ? UIComposition.Notification : UIComposition.Hidden)
 
   useEffect(() => {
     function loadAppId() {
